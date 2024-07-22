f098b589df86efba44bfa96efba0db3393af97a9
maardgs <mariliarodrigues.94@gmail.com>
2024-07-22 10:11:08 -0300

    Inserindo a Classe Categoria e o CRUD

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,39 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		
+		@NotBlank(message = "O nome da categoria é obrigatório")
+		private String nomeCategoria;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getNomeCategoria() {
+			return nomeCategoria;
+		}
+
+		public void setNomeCategoria(String nomeCategoria) {
+			this.nomeCategoria = nomeCategoria;
+		}
+		
+		
+
+}
